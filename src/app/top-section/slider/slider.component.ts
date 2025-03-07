a5ba2d7a2ad7edf73667cbcfe9155f5cca1e0ed3
mustapha.assoum <mustapha.assoum@leapdev.io>
2025-03-07 17:04:34 +1100

    added slider

--- src/app/top-section/slider/slider.component.ts
+++ src/app/top-section/slider/slider.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { Slider } from 'primeng/slider';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-slider',
-  imports: [],
+  imports: [Slider, FormsModule],
   templateUrl: './slider.component.html',
   styleUrl: './slider.component.css'
 })
 export class SliderComponent {
-
+  rangeValues: number[] = [20, 80];
 }
