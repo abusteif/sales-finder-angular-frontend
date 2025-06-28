c2430b176691cbeccd9496b32a5aa0c27e521aab
Mustapha <mustapha.assoum@hotmail.com>
2025-06-28 21:44:25 +1000

    fixed api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    apiBaseUrl: 'http://209.38.92.209/',
+    apiBaseUrl: 'http://209.38.92.209:8000',
     itemsPerPage: 8,
     maxPriceRange: [1,999],
     maxDiscountRange: [1,100],
