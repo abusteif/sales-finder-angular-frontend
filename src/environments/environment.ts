9673aea1305c885cda56c3597d9722c62debe8a1
Mustapha <mustapha.assoum@hotmail.com>
2025-07-06 13:27:01 +1000

    added jbhifi

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
     production: false,
     // apiBaseUrl: 'http://192.168.0.13:5000',
-      // apiBaseUrl: 'http://192.168.0.3:5000',
-      apiBaseUrl: 'http://209.38.92.209:8000',
+      apiBaseUrl: 'http://192.168.0.3:5000',
+      // apiBaseUrl: 'http://209.38.92.209:8000',
     maxPriceRange: [1,999],
     maxDiscountRange: [1,100],
     camelCamelCamelBaseUrl: 'https://au.camelcamelcamel.com'
