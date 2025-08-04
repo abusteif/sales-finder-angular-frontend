8cc67a6c8f5c994e721ec967b1383911c2739ba4
Mustapha <mustapha.assoum@hotmail.com>
2025-08-04 16:31:16 +1000

    add profile page placeholder

--- src/app/pages/profile-page/profile-page.component.ts
+++ src/app/pages/profile-page/profile-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NavigationService } from '../../core/services/navigation.service';
+import { AuthenticationStore } from '../../state/authentication.store';
 
 @Component({
   selector: 'app-profile-page',
@@ -7,5 +9,13 @@ import { Component } from '@angular/core';
   styleUrl: './profile-page.component.css'
 })
 export class ProfilePageComponent {
+  constructor(private navigationService: NavigationService, private authenticationStore: AuthenticationStore) {}
 
+  onBackToHome() {
+    this.navigationService.navigateToHome();
+  }
+
+  onViewAlerts() {
+    this.navigationService.navigateToProtectedRoute('/alerts', {}, this.authenticationStore.isAuthenticated());
+  }
 }
