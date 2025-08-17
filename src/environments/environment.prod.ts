b4854c0b5184c4a5fd6e80ae1a415d5851b3d784
Mustapha <mustapha.assoum@hotmail.com>
2025-08-17 22:14:14 +1000

    fixing api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    apiBaseUrl: 'https://bargainradar.com.au:8000/api',
+    apiBaseUrl: 'https://bargainradar.com.au/api',
     itemsPerPage: 8,
     maxPriceRange: [1,999],
     maxDiscountRange: [1,100],
