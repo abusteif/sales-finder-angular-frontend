587ea87bd42a701d8d0e95bb4a950ff8fee198c2
Mustapha <mustapha.assoum@hotmail.com>
2025-11-18 21:22:40 +1100

    remove bing lee from list of stores

--- src/app/core/services/store.service.ts
+++ src/app/core/services/store.service.ts
@@ -1,18 +1,22 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import {  map, Observable } from 'rxjs';
 import { environment } from '../../../environments/environment';
 import { Store } from '../models/store.model';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root',
 })
 export class StoreService {
-    private storesBaseUrl = `${environment.apiBaseUrl}/stores`;
+  private storesBaseUrl = `${environment.apiBaseUrl}/stores`;
 
-    constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {}
 
-    getStores(): Observable<Store[]> {
-        return this.http.get<Store[]>(this.storesBaseUrl);
-    }
+  getStores(): Observable<Store[]> {
+    return this.http
+      .get<Store[]>(this.storesBaseUrl)
+      .pipe(
+        map((stores) => stores.filter((store) => store.name !== 'bing lee'))
+      );
+  }
 }
