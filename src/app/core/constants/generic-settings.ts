8c4a62cd98e9440f430c0c1ada138d80fb1fe227
Mustapha <mustapha.assoum@hotmail.com>
2025-12-04 22:06:53 +1100

    disable maintenance flag

--- src/app/core/constants/generic-settings.ts
+++ src/app/core/constants/generic-settings.ts
@@ -4,6 +4,6 @@ export const GENERIC_SETTINGS = {
   defaultDescription: 'Bargain Radar helps you find, track, and snag the best deals from leading Australian retailers with smart alerts and real-time price monitoring.',
   defaultKeywords: 'bargain radar, sales finder, price tracker, deals, discounts, sales alerts, bargain hunting, price drops, shopping deals, Australia deals, online shopping, sale tracker, discount finder, bargain finder, price monitoring',
   socialImage: 'https://bargainradar.com.au/assets/logos/bargain-radar-logo.svg',
-  maintenanceMode: true
+  maintenanceMode: false
 };
 
